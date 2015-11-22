8eddce4ac1ec9d1f628243bab9455ab7f1ca8db2
ccisko <ccisko317@gmail.com>
2015-11-21 23:24:01 -0500

    fixed asybc problem

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -8,50 +8,28 @@ $(function() {
 	the allFeeds variable in our application.
      */
     describe('My RSS Feeds', function() {
-
         it('All Feeds are defined and not empty', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
-
-
-
         it('All feeds objects have URL defined and  not empty', function() {
             for (var i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].url).toBeDefined();
                 expect(allFeeds[i].url.length).not.toBe(0);
             }
-
-
         });
-
-
         it('All feeds objects have NAME defined and  not empty', function() {
             for (var i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].name).toBeDefined();
                 expect(allFeeds[i].name.length).not.toBe(0);
             }
-
-
         });
-
-
-
     });
 
-
-
     describe('The menu', function() {
-
-
-
         it('The menu should be hidden by Default', function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
-
-
-
-
         it('ensures menu changes visibility when the menu icon is clicked', function() {
             $('.menu-icon-link').trigger('click');
             expect($('body').hasClass('menu-hidden')).toBe(false);
@@ -59,7 +37,6 @@ $(function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
     });
-
 
     describe('Initial Entries', function() {
         var feedOne;
@@ -75,31 +52,22 @@ $(function() {
         });
     });
 
-
-
     describe('New Feed Selection', function() {
         var feedOne, feedTwo;
         beforeEach(function(done) {
-            $('.feed').empty();
             loadFeed(1, function() {
                 feedOne = $('.feed').find("h2").text();
+                done();
             });
-
+        });
+        beforeEach(function(done) {
             loadFeed(2, function() {
                 feedTwo = $('.feed').find("h2").text();
                 done();
             });
-
         });
-
         it('New feed content changes', function() {
             expect(feedOne).not.toEqual(feedTwo);
-
         });
-
-
     });
-
-
-
 }());
